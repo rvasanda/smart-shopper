2be77a69d0fff6ddf84192a390b01bc0b0e0312a
Whodat ninja <seven_up10@hotmail.com>
2014-08-02 22:32:36 -0400

    Setting up logging --IN PROGRESS

--- src/main/java/crawler/CrawlerMain.java
+++ src/main/java/crawler/CrawlerMain.java
@@ -1,5 +1,8 @@
 package crawler;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,13 +11,22 @@ import java.util.List;
  */
 public class CrawlerMain {
 
+    private static final Logger logger = LogManager.getLogger(CrawlerMain.class);
+
     public static void main(String[] args) {
         List<Crawler> crawlers = new ArrayList<Crawler>();
 
-        //crawlers.add(CrawlerFactory.createCustomCrawler(CrawlerFactory.Crawlers.BESTBUY));
+        crawlers.add(CrawlerFactory.createCustomCrawler(CrawlerFactory.Crawlers.BESTBUY));
         crawlers.add(CrawlerFactory.createCustomCrawler(CrawlerFactory.Crawlers.FUTURESHOP));
-
+        printAvailbleCrawlers(crawlers);
         ScheduledCrawlerService scheduledCrawlerService = new ScheduledCrawlerService(crawlers);
         scheduledCrawlerService.runCrawlerEveryHour();
+    }
+
+    private static void printAvailbleCrawlers(List<Crawler> availableCrawlers) {
+        logger.debug("sad");
+        for (Crawler c : availableCrawlers) {
+            logger.debug("sad");
+        }
     }
 }
