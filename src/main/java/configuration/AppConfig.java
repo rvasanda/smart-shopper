e67fd29943a4acfcb11cb8cb3789181c6f6e113b
Whodat ninja <seven_up10@hotmail.com>
2014-08-17 10:19:42 -0500

    Adding getters to retrieve tracked products and crawler details

--- src/main/java/configuration/AppConfig.java
+++ src/main/java/configuration/AppConfig.java
@@ -39,13 +39,14 @@ public class AppConfig {
     private static File configFile = null;
 
     static {
-        //readXMLConfigFile();
+        readXMLConfigFile();
         //readPropertiesFile();
     }
 
     private AppConfig() { }
 
     public static void readXMLConfigFile() {
+        logger.debug("hrroh");
         InputStream stream = null;
 
         try {
@@ -99,7 +100,6 @@ public class AppConfig {
                     product.details.put(detailsNode.getNodeName(), detailsNode.getTextContent());
                 }
                 trackedProducts.add(product);
-
             }
             logger.info("Configuration for " + PRODUCT_CONFIG_FILE + " read successfully");
         } catch (FileNotFoundException e) {
@@ -125,83 +125,6 @@ public class AppConfig {
         }
     }
 
-//    public static Map<String,Object> readXMLConfigFile(String filePath) {
-//
-//        if (filePath == null || filePath.isEmpty()) {
-//            logger.error("Filepath is null or empty!");
-//            return null;
-//        }
-//
-//        Map<String, Object> xmlProperties = new HashMap<String, Object>();
-//        InputStream stream = null;
-//
-//        try {
-//            stream = new FileInputStream(new File(filePath));
-//            DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-//            DocumentBuilder builder =  builderFactory.newDocumentBuilder();
-//            Document xmlDocument = builder.parse(stream);
-//            XPath xPath =  XPathFactory.newInstance().newXPath();
-//
-//            // Read Base URL
-//
-//            String expression = "/Configuration/BaseURL";
-//            Node baseUrlNode = (Node) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODE);
-//
-//            xmlProperties.put(baseUrlNode.getNodeName(), baseUrlNode.getTextContent());
-//
-//            // Read HTML Div Classes
-//
-//            expression = "Configuration/HTMLDivClass/*";
-//            NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
-//
-//            for (int i = 0; i < nodeList.getLength(); i++) {
-//                Node node = nodeList.item(i);
-//                xmlProperties.put(node.getNodeName(), node.getTextContent());
-//            }
-//
-//            // Read Tracked Products
-//
-//            expression = "Configuration/TrackedProducts/Product";
-//            nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
-//
-//            String childExpression = "*";
-//            for (int i = 0; i < nodeList.getLength(); i++) {
-//                Node node = nodeList.item(i);
-//                TrackedProduct product = new TrackedProduct();
-//                NodeList productDetails = (NodeList) xPath.compile(childExpression).evaluate(node, XPathConstants.NODESET);
-//
-//                for (int j = 0; j < productDetails.getLength(); ++j) {
-//                    Node detailsNode = productDetails.item(j);
-//                    product.details.put(detailsNode.getNodeName(), detailsNode.getTextContent());
-//                }
-//
-//                xmlProperties.put(product.details.get("Name"), product);
-//            }
-//            logger.info("Configuration for " + filePath + " read successfully");
-//        } catch (FileNotFoundException e) {
-//            logger.error(e.getMessage(), e);
-//        } catch (SAXException e) {
-//            logger.error(e.getMessage(), e);
-//        } catch (IOException e) {
-//            logger.error(e.getMessage(), e);
-//        } catch (ParserConfigurationException e) {
-//            logger.error(e.getMessage(), e);
-//        } catch (XPathExpressionException e) {
-//            logger.error(e.getMessage(), e);
-//        } catch (Exception e) {
-//            logger.error(e.getMessage(), e);
-//        } finally {
-//            try {
-//                if (stream != null) {
-//                    stream.close();
-//                }
-//            } catch (IOException e) {
-//                logger.error(e.getMessage(), e);
-//            }
-//        }
-//        return xmlProperties;
-//    }
-
     public static Properties readPropertiesFile(String filePath) {
         InputStream stream = null;
         Properties properties = null;
@@ -224,5 +147,13 @@ public class AppConfig {
             }
         }
         return properties;
+    }
+
+    public Set<TrackedProduct> getProducts() {
+        return trackedProducts;
+    }
+
+    public Set<CrawlerDetails> getCrawlerDetails() {
+        return crawlerDetails;
     }
 }
