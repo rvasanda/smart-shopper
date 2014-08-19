3e4853af69469e90bf360a86f4aefc6ddff42700
unknown <rvasandani@rvasandani.Destiny.local>
2014-08-19 17:41:29 -0400

    Removed custom crawler constructors

--- src/main/java/crawler/customcrawlers/FutureShopCrawler.java
+++ src/main/java/crawler/customcrawlers/FutureShopCrawler.java
@@ -9,12 +9,6 @@ import org.jsoup.nodes.Document;
  */
 public class FutureShopCrawler extends Crawler {
 
-    private static final String CONFIG_FILE = ConfigConstants.CONFIG_FOLDER + "FutureShopConfig.xml";
-
-    public FutureShopCrawler() {
-        super(CONFIG_FILE);
-    }
-
     @Override
     protected boolean checkIfTargetPage(Document pageDoc, String query) {
         return false;
